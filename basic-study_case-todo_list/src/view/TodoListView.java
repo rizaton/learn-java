ad6243a83596f17f25fece2f6920280fa5f32aa5
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-28 03:04:44 +0700

    View : Add Todo

--- basic-study_case-todo_list/src/view/TodoListView.java
+++ basic-study_case-todo_list/src/view/TodoListView.java
@@ -34,7 +34,15 @@ public class TodoListView {
         }
     }
     public void addTodoList(){
+        System.out.println("Add Todo List");
 
+        var todo = InputUtil.input("Todo (x for cancel)");
+
+        if (todo.equals("x")){
+            // cancel
+        } else {
+            todoListService.addTodoList(todo);
+        }
     }
     public void removeTodoList(){
 
