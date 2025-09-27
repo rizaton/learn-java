8554ce6470fc36dac05ff9a77919d64a40cc5029
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-28 03:02:17 +0700

    Test View : Show Todo

---
+++ basic-study_case-todo_list/src/test/view/TodoListViewTest.java
@@ -0,0 +1,24 @@
+package test.view;
+
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+import view.TodoListView;
+
+public class TodoListViewTest {
+    public static void main(String[] args) {
+        testShowTodoList();
+    }
+    public static void testShowTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Learn Basic Java");
+        todoListService.addTodoList("Learn Basic OOP");
+        todoListService.addTodoList("Learn Basic Standard Classes");
+
+        todoListView.showTodoList();
+    }
+}
