8f27069ca4bcf137ca735b78e49aed3727370732
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-28 02:39:29 +0700

    Repository & Service : Add Todo

--- basic-study_case-todo_list/src/repository/TodoListRepositoryImpl.java
+++ basic-study_case-todo_list/src/repository/TodoListRepositoryImpl.java
@@ -11,9 +11,41 @@ public class TodoListRepositoryImpl implements TodoListRepository{
         return data;
     }
 
+    public boolean isFull(){
+        var isFull = true;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                // there's still empty in model
+                isFull = false;
+                break;
+            }
+        }
+        return isFull;
+    }
+
+    public void resizeIfFull(){
+        // If full, model get resized two times
+        if (isFull()){
+            var temp = data;
+            data = new TodoList[data.length * 2];
+
+            for (int i = 0; i < temp.length; i++) {
+                data[i] = temp[i];
+            }
+        }
+    }
+
     @Override
     public void add(TodoList todoList) {
+        resizeIfFull();
 
+        // Add to a position that the array data is null
+        for (var i = 0; i < data.length; i++) {
+            if (data[i] == null){
+                data[i] = todoList;
+                break;
+            }
+        }
     }
 
     @Override
--- basic-study_case-todo_list/src/service/TodoListServiceImpl.java
+++ basic-study_case-todo_list/src/service/TodoListServiceImpl.java
@@ -28,7 +28,9 @@ public class TodoListServiceImpl implements TodoListService {
 
     @Override
     public void addTodoList(String todo) {
-
+        TodoList todoList = new TodoList(todo);
+        todoListRepository.add(todoList);
+        System.out.println("Success adding Todo : " + todo);
     }
 
     @Override
