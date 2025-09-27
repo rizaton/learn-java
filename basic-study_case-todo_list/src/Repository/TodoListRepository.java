7b150180a8e668338fdbe5bded5da0777cba48bc
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-27 23:12:58 +0700

    Repository

---
+++ basic-study_case-todo_list/src/Repository/TodoListRepository.java
@@ -0,0 +1,13 @@
+package Repository;
+
+import entity.TodoList;
+
+public interface TodoListRepository {
+
+    TodoList[] getAll();
+
+    void add(TodoList todoList);
+
+    void remove(Integer number);
+
+}
