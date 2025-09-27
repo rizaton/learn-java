354d3fa5573e1716100392aad10de7e40fba8c12
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-28 02:42:17 +0700

    Test Repository & Service : Add Todo

--- basic-study_case-todo_list/src/test/service/TodoListServiceTest.java
+++ basic-study_case-todo_list/src/test/service/TodoListServiceTest.java
@@ -1,13 +1,14 @@
 package test.service;
 
 import entity.TodoList;
+import repository.TodoListRepository;
 import repository.TodoListRepositoryImpl;
 import service.TodoListService;
 import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testShowTodoList();
+        testAddTodoList();
     }
 
     public static void testShowTodoList(){
@@ -15,9 +16,19 @@ public class TodoListServiceTest {
         todoListRepository.data[0] = new TodoList("Learn Basic Java");
         todoListRepository.data[1] = new TodoList("Learn Basic OOP");
         todoListRepository.data[2] = new TodoList("Learn Basic Standard Classes");
-
-
+        
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.showTodoList();
+    }
+
+    public static void testAddTodoList(){
+        TodoListRepository todoListRepository =  new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Learn Basic Java");
+        todoListService.addTodoList("Learn Basic OOP");
+        todoListService.addTodoList("Learn Basic Standard Classes");
 
         todoListService.showTodoList();
     }
