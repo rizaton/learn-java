92237b57a12702f9837c0a25221f62d90d82aa37
rizaton <123567427+rizaton@users.noreply.github.com>
2025-09-28 02:52:52 +0700

    Test Repository & Service : Remove Todo

--- basic-study_case-todo_list/src/test/service/TodoListServiceTest.java
+++ basic-study_case-todo_list/src/test/service/TodoListServiceTest.java
@@ -8,7 +8,7 @@ import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testAddTodoList();
+        testRemoveTodoList();
     }
 
     public static void testShowTodoList(){
@@ -16,7 +16,7 @@ public class TodoListServiceTest {
         todoListRepository.data[0] = new TodoList("Learn Basic Java");
         todoListRepository.data[1] = new TodoList("Learn Basic OOP");
         todoListRepository.data[2] = new TodoList("Learn Basic Standard Classes");
-        
+
         TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
 
         todoListService.showTodoList();
@@ -30,6 +30,30 @@ public class TodoListServiceTest {
         todoListService.addTodoList("Learn Basic OOP");
         todoListService.addTodoList("Learn Basic Standard Classes");
 
+        todoListService.showTodoList();
+    }
+
+    public static void testRemoveTodoList(){
+        TodoListRepositoryImpl todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Learn Basic Java");
+        todoListService.addTodoList("Learn Basic OOP");
+        todoListService.addTodoList("Learn Basic Standard Classes");
+
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(5);
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(2);
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(1);
         todoListService.showTodoList();
     }
 
