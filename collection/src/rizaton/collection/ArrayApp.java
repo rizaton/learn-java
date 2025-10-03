fe217bab9dc363ddbc33bad05ead2384c8e85b82
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-03 08:32:12 +0700

    Convert to Array

---
+++ collection/src/rizaton/collection/ArrayApp.java
@@ -0,0 +1,18 @@
+package rizaton.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayApp {
+    public static void main(String[] args) {
+
+        List<String> names = List.of("Andy", "Jane", "John", "Zane");
+
+        Object[] objects = names.toArray();
+        String[] strings = names.toArray(new String[]{});
+
+        System.out.println(Arrays.toString(objects));
+        System.out.println(Arrays.toString(strings));
+
+    }
+}
