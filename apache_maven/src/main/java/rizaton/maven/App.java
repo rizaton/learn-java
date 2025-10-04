ad03f127ec5bb6623ac301c3c4caf46982dcf4cc
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-04 13:36:32 +0700

    Maven Dependency

--- apache_maven/src/main/java/rizaton/maven/App.java
+++ apache_maven/src/main/java/rizaton/maven/App.java
@@ -1,10 +1,16 @@
 package rizaton.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Gson gson = new Gson();
+        Person person = new Person("Jack");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
     }
 }
