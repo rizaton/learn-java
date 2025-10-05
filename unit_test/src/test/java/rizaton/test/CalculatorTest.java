c4f7d9addafcc71b1cc9f38d85e346c511ea1d4b
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-05 07:38:36 +0700

    Renaming Test Name & Generating Test Name

--- unit_test/src/test/java/rizaton/test/CalculatorTest.java
+++ unit_test/src/test/java/rizaton/test/CalculatorTest.java
@@ -1,13 +1,20 @@
 package rizaton.test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
+import rizaton.test.generator.SimpleDisplayNameGenerator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayName("Test for Calculator")
+@DisplayNameGeneration(SimpleDisplayNameGenerator.class)
 public class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
 
     @Test
+//    @DisplayName("Scenario success test for add method add(Integer, Integer)")
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
         assertEquals(20, result);
