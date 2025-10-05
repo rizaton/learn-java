deef578b19912ed441ccb473c1b7ebf91a807ece
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-05 07:29:33 +0700

    Use Assertions

--- unit_test/src/main/java/rizaton/test/Calculator.java
+++ unit_test/src/main/java/rizaton/test/Calculator.java
@@ -2,6 +2,14 @@ package rizaton.test;
 
 public class Calculator {
 
+    public Integer divide(Integer first, Integer second) {
+        if (second == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        } else {
+            return first / second;
+        }
+    }
+
     public Integer add(Integer first, Integer second) {
         return first + second;
     }
--- unit_test/src/test/java/rizaton/test/CalculatorTest.java
+++ unit_test/src/test/java/rizaton/test/CalculatorTest.java
@@ -1,6 +1,7 @@
 package rizaton.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -9,5 +10,21 @@ public class CalculatorTest {
     @Test
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
+        assertEquals(20, result);
+
+    }
+
+    @Test
+    public void testDivideSuccess(){
+        var result = calculator.divide(100, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDivideFailed(){
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> calculator.divide(100, 0)
+        );
     }
 }
