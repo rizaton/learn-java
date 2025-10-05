bac3b45682cf5efbd0f7a33ea0d0b4df9006eefc
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-05 11:52:25 +0700

    Using Tag

---
+++ unit_test/src/test/java/rizaton/test/SampleIntegrationTest.java
@@ -0,0 +1,22 @@
+package rizaton.test;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+@Tags({
+        @Tag("integration-test")
+})
+public class SampleIntegrationTest {
+
+    @Test
+    void test1(){
+
+    }
+
+    @Test
+    void test2(){
+
+    }
+
+}
