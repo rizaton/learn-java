c4b93f691aa4f35ebf1332cc75970bfd6b286a48
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-05 11:18:41 +0700

    Test depending on Conditions

---
+++ unit_test/src/test/java/rizaton/test/ConditionTest.java
@@ -0,0 +1,84 @@
+package rizaton.test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.util.Properties;
+
+public class ConditionTest {
+
+    @Test
+    @Disabled
+    void testSystemProperties() {
+        Properties properties = System.getProperties();
+        properties.forEach((key, value) -> System.out.println(key + ": " + value));
+    }
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    public void testRunOnlyOnWindows(){
+        //
+    }
+
+    @Test
+    @DisabledOnOs({OS.WINDOWS})
+    public void testDisabledOnlyOnWindows(){
+        //
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_21})
+    public void testRunOnlyOnJava21(){
+        //
+    }
+
+    @Test
+    @DisabledOnJre({JRE.JAVA_21})
+    public void testDisabledOnlyOnJava21(){
+        //
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_15, max = JRE.JAVA_21)
+    public void testEnabledOnJava15to21(){
+        //
+    }
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_15, max = JRE.JAVA_21)
+    public void testDisabledOnJava15to21(){
+        //
+    }
+
+    @Test
+    @EnabledIfSystemProperties({
+            @EnabledIfSystemProperty(named = "java.vendor", matches = "Oracle Corporation")
+    })
+    public void testEnableOnOracle(){
+        //
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "java.vendor", matches = "Oracle Corporation")
+    public void testDisableOnOracle(){
+        //
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariables({
+            @EnabledIfEnvironmentVariable(named = "PROFILE", matches = "DEV")
+    })
+    public void testEnabledOnProfileDev(){
+        //
+    }
+
+    @Test
+    @DisabledIfEnvironmentVariables({
+            @DisabledIfEnvironmentVariable(named = "PROFILE", matches = "DEV")
+    })
+    public void testDisabledOnProfileDev(){
+        //
+    }
+
+}
