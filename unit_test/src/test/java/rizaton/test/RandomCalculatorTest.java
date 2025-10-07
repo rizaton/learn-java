3652323453dc91e1d4871ef79730da510a1edf57
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-07 18:49:04 +0700

    Repetitive Test

--- unit_test/src/test/java/rizaton/test/RandomCalculatorTest.java
+++ unit_test/src/test/java/rizaton/test/RandomCalculatorTest.java
@@ -1,7 +1,6 @@
 package rizaton.test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extensions;
 import rizaton.test.generator.RandomParameterResolver;
@@ -15,13 +14,15 @@ public class RandomCalculatorTest extends ParentCalculatorTest{
 
     private final Calculator calculator = new Calculator();
 
-    @Test
-    public void testRandom(Random random) {
+    @DisplayName("Test Calculator Random")
+    @RepeatedTest(
+            value = 3,
+            name = "{displayName} no.{currentRepetition} out of {totalRepetitions}"
+    )
+    public void testRandom(Random random, TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + " no." + repetitionInfo.getCurrentRepetition() + " out of " + repetitionInfo.getTotalRepetitions() );
         var a = random.nextInt();
         var b = random.nextInt();
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(calculator.add(a, b));
         Assertions.assertEquals(a + b, calculator.add(a, b));
     }
 
