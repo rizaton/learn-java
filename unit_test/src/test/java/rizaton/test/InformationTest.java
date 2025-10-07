30820a2f420e661aaf85fdbf780c7b1490970c70
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-07 18:33:56 +0700

    Test Information

---
+++ unit_test/src/test/java/rizaton/test/InformationTest.java
@@ -0,0 +1,20 @@
+package rizaton.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@DisplayName("Test with TestInfo")
+public class InformationTest {
+
+    @Test
+    @Tag("cool")
+    @DisplayName("This is test one")
+    public void testOne(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+        System.out.println(testInfo.getTags());
+        System.out.println(testInfo.getTestMethod());
+        System.out.println(testInfo.getTestClass());
+    }
+}
