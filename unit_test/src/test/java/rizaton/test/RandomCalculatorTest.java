b3478f4993b52b35cd3f4ff8fcba61b8b907b164
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-07 18:40:15 +0700

    Test with Dependency Injection

---
+++ unit_test/src/test/java/rizaton/test/RandomCalculatorTest.java
@@ -0,0 +1,28 @@
+package rizaton.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extensions;
+import rizaton.test.generator.RandomParameterResolver;
+
+import java.util.Random;
+
+@Extensions(value = {
+        @ExtendWith(RandomParameterResolver.class)
+})
+public class RandomCalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testRandom(Random random) {
+        var a = random.nextInt();
+        var b = random.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(calculator.add(a, b));
+        Assertions.assertEquals(a + b, calculator.add(a, b));
+    }
+
+}
