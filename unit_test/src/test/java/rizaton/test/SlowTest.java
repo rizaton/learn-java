7dc84a8fec1ec09c4b2f1f5103f990ddefd75b80
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-09 08:01:01 +0700

    Unit Test in Concurrent/Parallel mode

--- unit_test/src/test/java/rizaton/test/SlowTest.java
+++ unit_test/src/test/java/rizaton/test/SlowTest.java
@@ -2,14 +2,29 @@ package rizaton.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
-    public void slow() throws InterruptedException {
+    public void slowTest1() throws InterruptedException {
+        Thread.sleep(4000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void slowTest2() throws InterruptedException {
+        Thread.sleep(4000);
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    public void slowTest3() throws InterruptedException {
         Thread.sleep(4000);
     }
 
