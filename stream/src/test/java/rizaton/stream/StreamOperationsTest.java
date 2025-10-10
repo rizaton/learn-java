972a0fabae80923ad39eac80df0387ccd03bf0b6
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-10 14:17:39 +0700

    Stream Operations

---
+++ stream/src/test/java/rizaton/stream/StreamOperationsTest.java
@@ -0,0 +1,22 @@
+package rizaton.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOperationsTest {
+
+    @Test
+    void testMapStream() {
+
+        List<String> names = List.of("John", "Mary", "Robert", "Jane");
+
+        Stream<String> streamNames = names.stream();
+        Stream<String> streamUpper =  streamNames.map(String::toUpperCase);
+        
+        streamUpper.forEach(System.out::println);
+        names.forEach(System.out::println);
+    }
+}
