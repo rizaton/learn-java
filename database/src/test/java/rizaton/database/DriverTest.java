f942309078c732e0d6e5240a8157f6b6d278dd2b
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-12 11:43:13 +0700

    Java Database Intro, Driver

---
+++ database/src/test/java/rizaton/database/DriverTest.java
@@ -0,0 +1,20 @@
+package rizaton.database;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverTest {
+    @Test
+    void testRegister() {
+        try {
+            Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(mysqlDriver);
+        } catch (SQLException e) {
+            Assertions.fail(e);
+        }
+    }
+}
