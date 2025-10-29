a6d60680d08622d202c0a73ff4d63d28cd95972a
rizaton <123567427+rizaton@users.noreply.github.com>
2025-10-30 03:09:03 +0700

    Date & Time: ZoneId and ZoneOffset

---
+++ date-time/src/test/java/rizaton/datetime/ZoneTest.java
@@ -0,0 +1,34 @@
+package rizaton.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Set;
+
+public class ZoneTest {
+
+    @Test
+    void testCreateZoneId() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println(zoneId);
+
+        ZoneId zoneIdGMT = ZoneId.of("GMT");
+        System.out.println(zoneIdGMT);
+
+        Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
+        availableZoneIds.forEach(System.out::println);
+
+    }
+
+    @Test
+    void testZoneOffset() {
+        ZoneOffset zoneOffset1 = ZoneOffset.of("+07:00");
+        ZoneOffset zoneOffset2 = ZoneOffset.ofHours(7);
+        ZoneOffset zoneOffset3 = ZoneOffset.ofHoursMinutes(5,30);
+
+        System.out.println(zoneOffset1);
+        System.out.println(zoneOffset2);
+        System.out.println(zoneOffset3);
+    }
+}
