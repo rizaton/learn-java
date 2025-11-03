45d76edb14f4a8ef8392bfd6c653092f6f2f21c0
rizaton <123567427+rizaton@users.noreply.github.com>
2025-11-03 07:40:36 +0700

    Thread

---
+++ thread/src/main/java/rizaton/thread/LearningJavaThreadApplication.java
@@ -0,0 +1,13 @@
+package rizaton.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningJavaThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningJavaThreadApplication.class, args);
+	}
+
+}
---
+++ thread/src/test/java/rizaton/thread/LearningJavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package rizaton.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
