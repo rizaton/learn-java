2c58935928f9cfd94474c57462bc3fd0f849235e
rizaton <123567427+rizaton@users.noreply.github.com>
2025-11-03 10:54:09 +0700

    Thread: Race Condition

---
+++ thread/src/main/java/rizaton/thread/Counter.java
@@ -0,0 +1,13 @@
+package rizaton.thread;
+
+public class Counter {
+
+    private Long value = 0L;
+
+    public void increment() {
+        value++;
+    }
+    public Long getValue() {
+        return value;
+    }
+}
